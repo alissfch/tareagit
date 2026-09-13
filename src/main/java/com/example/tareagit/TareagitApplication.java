c1d4e0989c9e22a0f648e8559ba8ba793b9acb14
alisson <alissonfranco56@gmail.com>
2026-09-12 20:30:19 -0500

    first commit

---
+++ src/main/java/com/example/tareagit/TareagitApplication.java
@@ -0,0 +1,13 @@
+package com.example.tareagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareagitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareagitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tareagit/TareagitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tareagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareagitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
